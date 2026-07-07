5282f3cb4ea97fd7fb1091aa0ffefa8e6d74c51e
Oskar <122153079+Oskar-Erikssons@users.noreply.github.com>
2026-07-07 13:03:38 +0200

    Delete karlstad-analys directory

--- karlstad-analys/uppdatera-data.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-rem Uppdaterar all data i Karlstad-analys: hamtar fran SCB + Kolada,
-rem bygger om datalagret och skriver ny app\data.js.
-rem Ladda om app\index.html i webblasaren efterat.
-cd /d "%~dp0"
-powershell -ExecutionPolicy Bypass -File "pipeline\hamta-scb-data.ps1"
-echo.
-echo Klart! Ladda om app\index.html i webblasaren for att se ny data.
-pause
